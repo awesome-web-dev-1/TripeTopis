313335c26560a020bb05d686ca4c0e051fcfbdd1
KeenCoding12 <medhanyetsegay96@gmail.com>
2025-04-06 22:24:46 +0300

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const AnimatedRoutes = () => {
             transition={{ duration: 0.3 }}
             className="fixed inset-0 bg-white z-50 flex items-center justify-center"
           >
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primaryClr"></div>
           </motion.div>
         )}
       </AnimatePresence>
